a69d1c276861f940d11c6e4b450791a4f72f2377
git-qiyongqiang2012.gmail.com <qiyongqiang2012@gmail.com>
2014-11-12 10:52:33 +0800

    HELLO2

--- ActionBarDemo/src/com/commnet/actionbardemo/MainActivity.java
+++ ActionBarDemo/src/com/commnet/actionbardemo/MainActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView textview1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		String name="dd";
+		textview1 = (TextView)findViewById(android.R.id.text1);
+		textview1.setText(name);
 	}
 }
